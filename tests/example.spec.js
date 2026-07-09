74cabb7e62326de84265c119ed0c8e8ca6c21653
dcerniauskasBA <d.cerniauskas@ba.lt>
2026-07-09 09:44:03 +0300

    antras delete (#2)
    antras
    
    Co-authored-by: Deividas <dei.cerniauskas@gmail.com>

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -10,7 +10,4 @@ test('has title', async ({ page }) => {
 
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
 });
